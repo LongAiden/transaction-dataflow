ceeafd9840e61af0ace941b38240c1cd7b0cf2aa
LongAiden <the.fallen.knight.1995@gmail.com>
2025-03-24 00:51:27 +0700

    Clean up master branch

--- scripts/postgres_update_script.sql
+++
@@ -1,7 +0,0 @@
-UPDATE airflow.public.transaction_data
-SET "Sources" = 'Current Account'
-WHERE "Vendor" = 'Sending Out' 
- AND "Sources" = 'Credit Card';
-
-select * from airflow.public.transaction_data td 
-limit 10
